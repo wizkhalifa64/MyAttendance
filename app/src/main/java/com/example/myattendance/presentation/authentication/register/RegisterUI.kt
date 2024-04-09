fe3db741ad4248fe01b4a9bc22558bcabf4568d0
wizkhalifa64 <sona95986@gmail.com>
2024-04-09 08:31:39 +0530

    added ui

--- app/src/main/java/com/example/myattendance/presentation/authentication/register/RegisterUI.kt
+++ app/src/main/java/com/example/myattendance/presentation/authentication/register/RegisterUI.kt
@@ -1,20 +1,37 @@
 package com.example.myattendance.presentation.authentication.register
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun RegisterUI(){
+fun RegisterUI() {
+    Column(modifier = Modifier.padding(20.dp)) {
+        Spacer(modifier = Modifier.height(50.dp))
+        Text(
+            text = "Register",
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth(),
+            fontFamily = FontFamily.SansSerif,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
 
-    Text(text = "Register", style = MaterialTheme.typography.displaySmall)
-    Text(
-        text = "Let's start! Please enter your details", style = MaterialTheme.typography.labelLarge
-    )
-    Spacer(modifier = Modifier.height(30.dp))
-
+        Column(modifier = Modifier.padding(35.dp)) {
+            OutlinedTextField(value = "", onValueChange = {}, modifier = Modifier.fillMaxWidth())
+        }
+    }
 }
