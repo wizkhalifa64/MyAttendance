6a1a8208b91bb3363f1bf8d924eaa87c2c0ab46b
wizkhalifa64 <sona95986@gmail.com>
2024-04-11 01:36:14 +0530

    f

--- app/src/main/java/com/example/myattendance/presentation/authentication/register/RegisterUI.kt
+++ app/src/main/java/com/example/myattendance/presentation/authentication/register/RegisterUI.kt
@@ -1,11 +1,13 @@
 package com.example.myattendance.presentation.authentication.register
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -15,10 +17,12 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun RegisterUI() {
+    val viewModel = viewModel<RegisterViewModel>()
+    val state = viewModel.registerState
     Column(modifier = Modifier.padding(20.dp)) {
         Spacer(modifier = Modifier.height(50.dp))
         Text(
@@ -26,12 +30,45 @@ fun RegisterUI() {
             textAlign = TextAlign.Center,
             modifier = Modifier.fillMaxWidth(),
             fontFamily = FontFamily.SansSerif,
-            fontSize = 30.sp,
+            style = MaterialTheme.typography.displaySmall,
             fontWeight = FontWeight.Bold
         )
 
-        Column(modifier = Modifier.padding(35.dp)) {
-            OutlinedTextField(value = "", onValueChange = {}, modifier = Modifier.fillMaxWidth())
+        Column(modifier = Modifier.padding(30.dp)) {
+            OutlinedTextField(value = state.name,
+                onValueChange = { viewModel.handleChange(RegisterFormEvent.HandleNameChange(it)) },
+                modifier = Modifier.fillMaxWidth(),
+                label = {
+                    Text(text = "Full Name")
+                },
+                supportingText = { state.nameError?.let { Text(text = state.nameError) } })
+            OutlinedTextField(value = state.email,
+                onValueChange = { viewModel.handleChange(RegisterFormEvent.HandleEmailChange(it)) },
+                modifier = Modifier.fillMaxWidth(),
+                label = {
+                    Text(text = "Email")
+                }
+            ,supportingText = { state.emailError?.let { Text(text = state.emailError) } })
+            OutlinedTextField(value = state.password,
+                onValueChange = { viewModel.handleChange(RegisterFormEvent.HandlePasswordChange(it)) },
+                modifier = Modifier.fillMaxWidth(),
+                label = {
+                    Text(text = "Password")
+                },
+                supportingText = { state.passwordError?.let { Text(text = state.passwordError) } })
+            OutlinedTextField(value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                label = {
+                    Text(text = "Location")
+                },
+                supportingText = {state.locationError?.let { Text(text = state.locationError) }}
+            )
+            Row(modifier = Modifier.fillMaxWidth(), Arrangement.Center) {
+                Button(onClick = {viewModel.handleChange(RegisterFormEvent.HandleRegisterSubmit)}) {
+                    Text(text = "Let's start")
+                }
+            }
         }
     }
 }
